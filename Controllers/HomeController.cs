d08f086d875e74046dde2e11785bff0ccbe433d6
florian <la238872@student.helha.be>
2026-01-06 10:38:15 +0100

    add privacy page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,6 +48,11 @@ public class HomeController : Controller
         return View(versionItems);
     }
 
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
